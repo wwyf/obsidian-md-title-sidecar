04797d64ec19024585c7897db0c9ff7c956bdfa1
Dylan Elliott <dylan.elliott@gmail.com>
2021-08-14 10:17:41 +1000

    August 14, 2021 10:17 AM

--- src/folder-title.ts
+++ src/folder-title.ts
@@ -23,7 +23,7 @@ export const setTitle = (item: FileItem, vault: Vault) => {
 
     let idMatch = item.file.basename.match(/([0-9]+|[a-z]+)/g)!
     if (idMatch) {
-        item
+        item.titleEl.dataset['zettel-indent'] = (idMatch.length - 1).toString()
     }
 
     vault.read(item.file).then(function (val) {
