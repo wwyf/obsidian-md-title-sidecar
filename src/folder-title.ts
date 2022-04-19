0a324c33e34243ff6540b2f9ca023858ac7acb31
WYF <w.y.ff@163.com>
2022-04-19 21:17:53 +0800

    update

--- src/folder-title.ts
+++ src/folder-title.ts
@@ -6,22 +6,27 @@ import { AFItem, FileItem, parseYaml, Vault } from 'obsidian';
 
 const detectTitle = (content: string) => {
     // First look into YAML Frontmatters
+    var return_string = "";
     const yfmMatch = /---[\r\n]+(.+?)[\r\n]+(---|\.\.\.)/s.exec(content);
     if (yfmMatch !== null) {
         const yamlStr = yfmMatch[1];
         const yfm = parseYaml(yamlStr);
         if (yfm.title) {
-            return yfm.title;
+            return_string = yfm.title;
         }
     }
 
     // Then look into H1
     const h1Match = /# (.+)\s*/.exec(content);
     if (h1Match != null) {
-        return h1Match[1];
+        return_string = h1Match[1];
     }
 
-    return '';
+    // if (return_string.length > 20) {
+    //     return_string = return_string.slice(0, 20) + '...';
+    // }
+
+    return return_string;
 };
 
 export const setupTitle = (
@@ -43,24 +48,46 @@ export const setTitle = (item: FileItem, vault: Vault) => {
         return;
     }
 
-    let idMatch = item.file.basename.match(/([0-9]+|[a-z]+)/g)!;
-    if (idMatch) {
-        let indentCount = idMatch.length - 1;
-        let indentStr = (indentCount * 20).toString() + 'px';
-        item.titleEl.style.marginLeft = indentStr;
+    let isFilenameID =  item.file.basename.match(/^[0-9]*$/g)!;
+    if (!isFilenameID) {
+        return;
     }
+
+    // let idMatch = item.file.basename.match(/([0-9]+|[a-z]+)/g)!;
+    // if (idMatch) {
+    //     let indentCount = idMatch.length - 1;
+    //     let indentStr = (indentCount * 20).toString() + 'px';
+    //     // item.titleEl.style.marginLeft = indentStr;
+    // }
 
     vault
         .read(item.file)
         .then(function (content) {
             const contentTitle = detectTitle(content);
             if (contentTitle) {
-                item.titleInnerEl.textContent = contentTitle;
+                // item.titleInnerEl.textContent = "";
+                item.titleInnerEl.textContent = item.file.basename;
 
-                const originalTitle = document.createElement('em');
-                originalTitle.className = 'folder-filename-title';
-                originalTitle.textContent = item.file.basename;
-                item.titleInnerEl.append(originalTitle);
+                // original title (it 's uid)
+                // const originalTitle = document.createElement('em');
+                // originalTitle.className = 'filename-uid';
+                // originalTitle.textContent = item.file.basename;
+
+
+                const nextLine = document.createElement('br');
+
+                var newTitleDiv = document.createElement('div');
+                newTitleDiv.className = "filename-uid-with-title";
+                // newTitleDiv.append(originalTitle);
+                // newTitleDiv.append(nextLine);
+                newTitleDiv.append(contentTitle);
+
+                // item.titleInnerEl.append(originalTitle);
+                item.titleInnerEl.append(newTitleDiv);
+
+                // item.titleInnerEl.prepend(nextLine);
+                // item.titleInnerEl.prepend(originalTitle);
+
             }
         })
         .catch((error) => {
